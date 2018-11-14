31947be1acc17d99b280d0281d39f30017582472
Vadym <vadym.vikulin@gmail.com>
2018-11-14 20:30:47 +0200

    1. static bt client

--- bt-streaming-service/src/main/java/org/hyperborian/bt/service/HttpStreamingTorrentService.java
+++ bt-streaming-service/src/main/java/org/hyperborian/bt/service/HttpStreamingTorrentService.java
@@ -7,17 +7,17 @@ import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.function.Consumer;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.StreamingOutput;
@@ -38,9 +38,11 @@ import bt.torrent.selector.SequentialSelector;
 @Path(value = "/")
 public class HttpStreamingTorrentService implements Consumer<TorrentSessionState>{
 	
+	private final int chunk_size = 1024 * 1024 * 2; // 2 MB chunks
+	
 	
 	protected long chunkSize;
-	private BtClient client;
+	public static BtClient client;
 	private int piecesAvailable;
 	protected String name;
 	protected long size;
@@ -51,52 +53,57 @@ public class HttpStreamingTorrentService implements Consumer<TorrentSessionState
 
 	@GET
 	@Path("/torrent/{infoHash}")
-	public Response getTorrent(@PathParam("infoHash") @NotNull @Size(min = 40, max = 40) @Pattern(regexp = "^[a-fA-F0-9]+$") String infoHash)  throws Exception  {
+	@Produces("video/mp4")
+	public Response getTorrent(@HeaderParam("Range") String range, @PathParam("infoHash") @NotNull @Size(min = 40, max = 40) @Pattern(regexp = "^[a-fA-F0-9]+$") String infoHash)  throws Exception  {
 		
 		//String magnetUri = "magnet:?xt=urn:btih:a9b09d61aaa9090a5fa77f7da02bcd78b80f6f85&dn=example.torrent";
-		String magnetUri = "magnet:?xt=urn:btih:"+infoHash;
-		pathToFile = Paths.get("Download");
-		storage = new StreamingFileSystemStorage(pathToFile);
-    	PieceSelector selector = SequentialSelector.sequential();
-    	Module module = new DHTModule (new DHTConfig() {
-    		@Override
-    		public boolean shouldUseRouterBootstrap() {
-    			return true;
-    		}
-    		
-    		@Override
-    		public boolean shouldUseIPv6() {
-    			return false;
-    		}
-    	});
-    	Consumer<Torrent> torrentConsumer = new Consumer<Torrent>() {
+		
+    	
+    	if(client==null) {
+    		String magnetUri = "magnet:?xt=urn:btih:"+infoHash;
+    		pathToFile = Paths.get("Download");
+    		storage = new StreamingFileSystemStorage(pathToFile);
+        	PieceSelector selector = SequentialSelector.sequential();
+        	Module module = new DHTModule (new DHTConfig() {
+        		@Override
+        		public boolean shouldUseRouterBootstrap() {
+        			return true;
+        		}
+        		
+        		@Override
+        		public boolean shouldUseIPv6() {
+        			return false;
+        		}
+        	});
+        	Consumer<Torrent> torrentConsumer = new Consumer<Torrent>() {
 
-			@Override
-			public void accept(Torrent t) {
-				HttpStreamingTorrentService.this.chunkSize = t.getChunkSize();
-				HttpStreamingTorrentService.this.name = t.getName();
-				HttpStreamingTorrentService.this.size = t.getSize();
-				HttpStreamingTorrentService.this.totalChunks = (int) Math.ceil(size / chunkSize);
-			}
-    		
-    	};
-		client = Bt.client().storage(storage).autoLoadModules().module(module).selector(selector).magnet(magnetUri).afterTorrentFetched(torrentConsumer).build();
-	
-		client.startAsync(this, 1000);
+    			@Override
+    			public void accept(Torrent t) {
+    				HttpStreamingTorrentService.this.chunkSize = t.getChunkSize();
+    				HttpStreamingTorrentService.this.name = t.getName();
+    				HttpStreamingTorrentService.this.size = t.getSize();
+    				HttpStreamingTorrentService.this.totalChunks = (int) Math.ceil(size / chunkSize);
+    			}
+        		
+        	};
+    		client = Bt.client().storage(storage).autoLoadModules().module(module).selector(selector).magnet(magnetUri).afterTorrentFetched(torrentConsumer).build();
+    		client.startAsync(this, 1000);
+    	}
+		
     
 	    while(size==0) {
 	    	Thread.sleep(200);
 	    }
 	    
-	    TimerTask timerTask = new MyTimerTask();
-        //running timer task as daemon thread
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask, 0, 1000);
-        System.out.println("TimerTask started");
+	    while(piecesAvailable==0) {
+	    	Thread.sleep(1000);
+	    }
 	    
 	    StreamingOutput stream = new StreamingOutput() {
 	    	
 			private long piecesSent = 0;
+			
+			
 			
 			java.nio.file.Path path = Paths.get(pathToFile.toString(), name);
 			SeekableByteChannel sbc = Files.newByteChannel(path, StandardOpenOption.READ);
@@ -105,7 +112,7 @@ public class HttpStreamingTorrentService implements Consumer<TorrentSessionState
 	            	while(true) {
 			            while(piecesAvailable==piecesSent) {
 			            	try {
-								Thread.sleep(500);
+								Thread.sleep(100);
 							} catch (InterruptedException e) {
 								e.printStackTrace();
 							}
@@ -134,8 +141,9 @@ public class HttpStreamingTorrentService implements Consumer<TorrentSessionState
 	    };
 	    
 	    ResponseBuilder response = Response.ok((Object) stream);
-        response.header("Content-Disposition", "attachment; filename=\""+name+"\"");
-        response.header("Content-Length", size);
+	    response.header("Content-Disposition", "attachment; filename=\""+name+"\"");
+	    response.header("Content-Type", "video/mp4");
+        response.header(HttpHeaders.CONTENT_LENGTH, size);
 
 	    return response.build();
 
@@ -153,23 +161,6 @@ public class HttpStreamingTorrentService implements Consumer<TorrentSessionState
 			
 			
 		}
-	}
-	
-	public class MyTimerTask extends TimerTask {
-
-	    @Override
-	    public void run() {
-	        System.out.println("Timer task started at:"+new Date());
-	        completeTask();
-	        System.out.println("Timer task finished at:"+new Date());
-	    }
-
-	    private void completeTask() {
-
-			System.err.println("Current offset:"+storage.getTorrentStorageUnit().getCurrentOffset());
-			
-	    }
-	    
 	}
 
 }
